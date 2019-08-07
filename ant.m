bb9487dd4596d7d24b3c40377f42b85e54af35dc
chemPolonium <li.boyuan@outlook.com>
2019-08-07 22:38:12 +0800

    Add files via upload

---
+++ ant.m
@@ -0,0 +1,29 @@
+mapSize = 1000;
+centerPosition = round(mapSize/2);
+[xCoordinates,yCoordinates] = meshgrid(0:mapSize-1,0:mapSize-1);
+map = false(mapSize);
+position = [centerPosition centerPosition];
+direction = [1 0];
+rotateLeft = @(x) [-x(2) x(1)];
+rotateRight = @(x) [x(2) -x(1)];
+
+for i = 1:11000
+    if map(position(1),position(2))
+        direction = rotateRight(direction);
+    else
+        direction = rotateLeft(direction);
+    end
+    map(position(1),position(2)) = ~map(position(1),position(2));
+    position = position + direction;
+end
+
+xBlackCoordinates = xCoordinates(map);
+yBlackCoordinates = yCoordinates(map);
+
+for ii = 1:length(xBlackCoordinates(:))
+    x = xBlackCoordinates(ii) + [0 1 1 0];
+    y = yBlackCoordinates(ii) + [0 0 1 1];
+    patch(x,y,'k','facealpha',1);
+end
+
+axis equal
